47e24fed775af5a09db956157553d51d55e9e148
kochnevaMRK <98736570+kochnevaMRK@users.noreply.github.com>
2022-02-08 11:02:19 +0300

    feat: update markup, style and pdf-file

--- index.js
+++ index.js
@@ -7,44 +7,62 @@ function slideUp () {
     document.body.classList.add('transform');
 }
 
-one.addEventListener('click', () => {
-    slideUp();
-    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 1.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
-})
-
-two.addEventListener('click', () => {
-    slideUp();
-    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 2.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
-})
-
-three.addEventListener('click', () => {
-    slideUp();
-    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 3.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
-})
-
-four.addEventListener('click', () => {
-    slideUp();
-    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 4.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
-})
-
-
 const onePDF = document.getElementById("1-part");
 const twoPDF = document.getElementById("2-part");
 const threePDF = document.getElementById("3-part");
 const fourPDF = document.getElementById("4-part");
 
+one.addEventListener('click', () => {
+    slideUp();
+    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 1.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    onePDF.style.backgroundColor = `rgb(164 231 121)`;
+})
+
+two.addEventListener('click', () => {
+    slideUp();
+    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 2.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    twoPDF.style.backgroundColor = `rgb(164 231 121)`;
+})
+
+three.addEventListener('click', () => {
+    slideUp();
+    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 3.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    threePDF.style.backgroundColor = `rgb(164 231 121)`;
+})
+
+four.addEventListener('click', () => {
+    slideUp();
+    document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 4.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    fourPDF.style.backgroundColor = `rgb(164 231 121)`;
+})
+
+function clear () {
+    onePDF.style.backgroundColor = `unset`;
+    twoPDF.style.backgroundColor = `unset`;
+    threePDF.style.backgroundColor = `unset`;
+    fourPDF.style.backgroundColor = `unset`;
+}
+
 onePDF.addEventListener('click', () => {
     document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 1.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    clear();
+    onePDF.style.backgroundColor = `rgb(164 231 121)`;
 })
 
 twoPDF.addEventListener('click', () => {
     document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 2.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    clear();
+    twoPDF.style.backgroundColor = `rgb(164 231 121)`;
 })
 
 threePDF.addEventListener('click', () => {
     document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 3.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    clear();
+    threePDF.style.backgroundColor = `rgb(164 231 121)`;
 })
 
 fourPDF.addEventListener('click', () => {
     document.getElementById("PDFwrap").innerHTML = `<iframe src="pdf/Раздел 4.pdf" style="width: 100%;height: 100%;border: none;"></iframe>`;
+    clear();
+    fourPDF.style.backgroundColor = `rgb(164 231 121)`;
 })
